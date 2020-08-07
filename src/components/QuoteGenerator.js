469233e1c580c8abd363701d2d526e567217eee2
artikgupta <artig9262@gmail.com>
2020-08-07 22:52:32 +0530

    made random quote generator

---
+++ src/components/QuoteGenerator.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+class QuoteGenerator extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      quote:
+        "I love deadlines. I like the whooshing sound they make as they fly by.",
+      author: "Douglas Adams",
+    };
+  }
+
+  quoteGenerationHandler = () => {
+    this.setState({
+      loading: true,
+    });
+    fetch("http://quotes.stormconsultancy.co.uk/random.json")
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          loading: false,
+          quote: data.quote,
+          author: data.author,
+        })
+      );
+  };
+
+  render() {
+    const { quote, author } = this.state;
+
+    return (
+      <div className="main-container">
+        <div className="container">
+          <div className="btn">
+            <button onClick={this.quoteGenerationHandler}>
+              Generate Quote
+            </button>
+          </div>
+          <div className="flex quote">
+            {this.state.loading === true ? (
+              <div className="center">
+                <i className="fa fa-cog fa-spin" />
+              </div>
+            ) : (
+              `${quote} - ${author}`
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuoteGenerator;
